5a3e243719bd965a98933a511913e3c773297c26
Diovane Pacheti <negociospacheti@gmail.com>
2022-10-09 14:02:39 -0300

    :art: add exemple method http

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,30 @@
-import express from 'express'
+import express, { request, response } from 'express'
 
 const app = express()
 
-app.get('/ads', (request, response) =>{
-    return response.json([
+app.get('/games', (request, response) =>{
+    return response.status(200).json([]);
+})
+
+app.post('/ads', (request, response) =>{
+    return response.status(201).json([]);
+})
+
+app.get('/games/:id/ads', (request, response) =>{
+    const gameId = request.params.id;
+
+    return response.status(200).json([
         {id:1, name:'Anuncio 1'},
         {id:2, name:'Anuncio 2'},
         {id:3, name:'Anuncio 3'}
+    ])
+})
+
+app.get('/ads/:id/discord', (request, response) =>{
+    // const adId = request.params.id;
+
+    return response.status(200).json([
+       
     ])
 })
 
