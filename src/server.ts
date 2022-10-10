b360fd50438686420f8907dace33630934d9a939
Diovane Pacheti <negociospacheti@gmail.com>
2022-10-10 02:40:42 -0300

    :art: add function converter minutes to string

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { PrismaClient } from '@prisma/client'
 import { convertHourStringToMinutes } from './utils/convert-hour-string-to-minutes'
+import { convertMinutesToHourString } from './utils/convert-minutes-to-hour-string'
 
 const app = express()
 
@@ -71,7 +72,9 @@ app.get('/games/:id/ads', async(request, response) =>{
     return response.status(200).json(ads.map(ad => {
         return {
             ...ad,
-            weekDays: ad.weekDays.split(',')
+            weekDays: ad.weekDays.split(','),
+            hourEnd: convertMinutesToHourString(ad.hourEnd),
+            hourStart: convertMinutesToHourString(ad.hourStart)
         }
     }))
 })
