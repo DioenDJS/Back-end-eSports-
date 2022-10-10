d879bedf48d7a957a79548cd55d05f4d18f84367
Diovane Pacheti <negociospacheti@gmail.com>
2022-10-09 23:57:41 -0300

    :art: create method find ads

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,15 @@ const prisma = new PrismaClient({
 
 app.get('/games', async (request, response) =>{
 
-    const games = await prisma.game.findMany()
+    const games = await prisma.game.findMany({
+        include: {
+            _count: {
+                select: {
+                    ads: true
+                }
+            }
+        }
+    })
 
     return response.status(200).json(games);
 })
@@ -17,14 +25,34 @@ app.post('/ads', (request, response) =>{
     return response.status(201).json([]);
 })
 
-app.get('/games/:id/ads', (request, response) =>{
+app.get('/games/:id/ads', async(request, response) =>{
     const gameId = request.params.id;
 
-    return response.status(200).json([
-        {id:1, name:'Anuncio 1'},
-        {id:2, name:'Anuncio 2'},
-        {id:3, name:'Anuncio 3'}
-    ])
+    const ads = await prisma.ad.findMany({
+        select: {
+            id: true,
+            name: true,
+            weekDays: true,
+            useVoiceChannel: true,
+            yearsPlaying: true,
+            hourStart: true,
+            hourEnd: true,
+        },
+
+        where: {
+            gameId,
+        },
+
+        orderBy: {
+            createdAt: 'desc'
+        }
+    })
+    return response.status(200).json(ads.map(ad => {
+        return {
+            ...ad,
+            weekDays: ad.weekDays.split(',')
+        }
+    }))
 })
 
 app.get('/ads/:id/discord', (request, response) =>{
